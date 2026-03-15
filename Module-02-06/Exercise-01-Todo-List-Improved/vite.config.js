96d10baf56565aa92b5c67091104cb45bb784f1f
Hendrik Tandiono <ahen.radiant@gmail.com>
2026-03-15 20:05:26 +0700

    feat: Initialize project with Vite configuration and build output.

--- Module-02-06/Exercise-01-Todo-List-Improved/vite.config.js
+++ Module-02-06/Exercise-01-Todo-List-Improved/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Purwadhika/Module-02-06/Exercise-01-Todo-List-Improved/',
+  base: './',
   plugins: [react()],
 })
