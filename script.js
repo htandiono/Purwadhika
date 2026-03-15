e503da3a079e662a4ff8c90b61bc6f54685b7e43
Hendrik Tandiono <ahen.radiant@gmail.com>
2026-03-15 20:12:48 +0700

    feat: Add 'Exercise-01-Todo-List-Improved' to the list of exercises that use 'dist/index.html'.

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ async function renderModuleCard(moduleItem) {
                 // Construct the local relative path to the index.html
                 let indexPath = 'index.html';
                 // Direct specific built projects to their dist folder
-                if (exercise.name === 'Exercise-01-Todo-List' || exercise.name === 'Exercise-01-Profile-Page') {
+                if (exercise.name === 'Exercise-01-Todo-List' || exercise.name === 'Exercise-01-Profile-Page' || exercise.name === 'Exercise-01-Todo-List-Improved') {
                     indexPath = 'dist/index.html';
                 }
 
